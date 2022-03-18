dd8cbc64a1dbe32610c06a91dc450de4f772997e
Victor <victorcost_@outlook.com>
2022-03-17 22:18:46 -0300

    feat: normalize

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from pandas import read_json
+from pandas import json_normalize
 from os import path
+from salvar_ajustar.salvar_ajustar import escolher_arquivo
+from salvar_ajustar.salvar_ajustar import salvar_arquivo_planilha
+import json
+from pprint import pprint
+import xlsxwriter
 
 
 def transform():
-    from salvar_ajustar.salvar_ajustar import escolher_arquivo
+    """useful when the json data from an scraper/extraction contains \n (10) \r (13) ASCIICODE and amount of line breaks,
+    then the text is splitted and this problem is solved
+    https://stackoverflow.com/questions/1761051/difference-between-n-and-r/1761086"""
+
     file_local = escolher_arquivo()
     file_name = path.basename(file_local)
     json_file = read_json(file_local)
@@ -19,5 +28,15 @@ def transform():
     print(f'Arquivo salvo em {local_save}')
 
 
+def read_json_extra_data_problem():
+    """this was used when I tried to read a json file that was reporting me a Extra Data file, what means that there are
+    two json data in the file"""
+    test = []
+    for line in open(escolher_arquivo(), 'r'):
+        test.append(json.loads(line))
+    df = json_normalize(test)
+    salvar_arquivo_planilha(df, 'dutra', 'xlsx')
+
+
 if __name__ == '__main__':
-    transform()
+    read_json_extra_data_problem()
