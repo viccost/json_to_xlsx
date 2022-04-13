890d72b05b26ba09b54f9c5eee602892ae04f18c
Victor <victorcost_@outlook.com>
2022-04-12 22:58:01 -0300

    documenting module

--- salvar_ajustar/salvar_ajustar.py
+++ salvar_ajustar/salvar_ajustar.py
@@ -1,3 +1,11 @@
+"""
+Module for helps to handling excel files. As save multiple archives and
+generate dataframes.
+
+    Written by Victor Costa (victorcost_@outlook.com).
+    version 1.0
+"""
+
 import pandas as pd
 import xlsxwriter
 
@@ -6,12 +14,13 @@ def escolher_arquivo() -> str:
     """Abre uma janela para que o usuário possa escolher QUALQUER ARQUIVO."""
     import tkinter as tk
     from tkinter import filedialog
+
     # Talvez setar os formatos específicos seja uma bom upgrade.
     root = tk.Tk()
-    root.attributes('-topmost', 1)
+    root.attributes("-topmost", 1)
     root.withdraw()
     file_path = filedialog.askopenfilename()
-    if file_path == '':
+    if file_path == "":
         print("Encerrando")
         return exit()
     return file_path
@@ -21,36 +30,42 @@ def escolher_pasta() -> str:
     """Abre uma janela para que o usuário possa escolher QUALQUER ARQUIVO."""
     import tkinter as tk
     from tkinter import filedialog
+
     # Talvez setar os formatos específicos seja uma bom upgrade.
     root = tk.Tk()
-    root.attributes('-topmost', 1)
+    root.attributes("-topmost", 1)
     root.withdraw()
     folder_path = filedialog.askdirectory()
-    if folder_path == '':
+    if folder_path == "":
         print("Encerrando")
         return exit()
     return folder_path
 
 
-def gerar_dataframe(file_path, name_sheet: 0) -> pd.DataFrame:
+def gerar_dataframe(file_path, name_sheet= 0) -> pd.DataFrame:
     """Espera um arquivo .xls, .xlsx, .csv para converter para um pandas.DataFrame e retorná-lo"""
     # checar tipo do arquivo ou trycatch para tratar erros
     planilha = pd.read_excel(file_path, index_col=None, sheet_name=name_sheet)
     return planilha
 
 
-def salvar_arquivo_planilha(planilha: pd.DataFrame, nome: str, formato: str, folder: str = '') -> None:
+def salvar_arquivo_planilha(
+    planilha: pd.DataFrame, nome: str, formato: str, folder: str = ""
+) -> None:
     """Recebe um DataFrame e nome do arquivo para salvá-lo como .xlsx. Ajusta automaticamente o tamanho das colunas.
     Index do DataFrame está setado como falso. E os campos em branco estão mantidos em branco sem alteração."""
     _formato = formato
 
     def checar_caminho() -> None:
         from os import path
+
         if path.exists(caminho_desktop):
             pass
         else:
-            print("Erro: Defina corretamente o caminho até a pasta que deseja salvar suas planilhas!\n"
-                  "Encerrando.")
+            print(
+                "Erro: Defina corretamente o caminho até a pasta que deseja salvar suas planilhas!\n"
+                "Encerrando."
+            )
             exit()
 
     def salvar() -> None:
@@ -59,9 +74,11 @@ def salvar_arquivo_planilha(planilha: pd.DataFrame, nome: str, formato: str, fol
         if _formato == "xls":
             engine = "xlwt"
 
-        writer = pd.ExcelWriter(rf'{caminho_desktop}\{nome}.{_formato}', engine=f'{engine}')
+        writer = pd.ExcelWriter(
+            rf"{caminho_desktop}\{nome}.{_formato}", engine=f"{engine}"
+        )
         # engine_kwargs={'options': {'strings_to_numbers': True}})
-        planilha.to_excel(writer, sheet_name=f'{nome}', index=False)
+        planilha.to_excel(writer, sheet_name=f"{nome}", index=False)
         try:
             writer.save()
             print(f'A planilha "{nome}" foi criada!')
@@ -69,7 +86,7 @@ def salvar_arquivo_planilha(planilha: pd.DataFrame, nome: str, formato: str, fol
             print(f"Erro ao salvar o arquivo: {err}")
             exit()
 
-    if folder == '':
+    if folder == "":
         caminho_desktop = escolher_pasta()
     else:
         caminho_desktop = folder
@@ -83,6 +100,6 @@ def salvar_planilhas(df_para_salvar: list):
     :param df_para_salvar: list -> [{},{}]"""
     caminho_desktop = escolher_pasta()
     for df in df_para_salvar:
-        nome = df['Nome']
-        modelo_dataframe = df['Dataframe']
-        salvar_arquivo_planilha(modelo_dataframe, nome, 'xlsx', caminho_desktop)
+        nome = df["Nome"]
+        modelo_dataframe = df["Dataframe"]
+        salvar_arquivo_planilha(modelo_dataframe, nome, "xlsx", caminho_desktop)
